3af2ec8177bf4d42bd0ac08fec136027d9dae1fa
pabloaledolopez7 <pabloaledolopez7@gmail.com>
2026-03-20 17:26:19 +0100

    Añadir método fecha y hora

--- Principal.java
+++ Principal.java
@@ -1,5 +1,13 @@
+import java.time.LocalDateTime;
+
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		mostrarFechaHora();
+	}
+
+	public static void mostrarFechaHora() {
+		LocalDateTime ahora = LocalDateTime.now();
+		System.out.println("Fecha y hora actual: " + ahora);
 	}
 }
