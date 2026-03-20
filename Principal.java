75c462fdce17ffb8962a373ffb426f663fd7972f
pabloaledolopez7 <pabloaledolopez7@gmail.com>
2026-03-20 17:52:04 +0100

    Implementar usuarios y array

--- Principal.java
+++ Principal.java
@@ -1,13 +1,43 @@
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		mostrarFechaHora();
+
+		Usuario[] usuarios = crearUsuarios(2);
+
+		for (usuario u : usuarios) {
+			System.out.println(u.nombre + " " + u.apellidos + " - " + u.email);
+		}
 	}
 
 	public static void mostrarFechaHora() {
 		LocalDateTime ahora = LocalDateTime.now();
 		System.out.println("Fecha y hora actual: " + ahora);
 	}
+
+	public static Usuario[] crearUsuarios(int n) {
+		Scanner sc = new Scanner(System.in);
+		Usuario[] usuarios = new Usuario[n];
+
+		for (int i = 0; i < n; i ++) {
+			Usuario u = new Usuario();
+
+			System.out.println("Nombre:");
+			u.nombre = sc.nextLine();
+
+			System.out.println("Apellidos:");
+			u.apellidos = sc.nextLine();
+
+			System.out.println("Email:");
+			u.email = sc.nextLine();
+
+			usuarios[i] = u;
+		}
+
+		return usuarios;
+	}
 }
+
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Proyecto Git
 
-Este proyecto contiene un programa en Java que imprime "Hola munso"
+Este proyecto contiene un programa en Java que incluye las siguientes funcionalidades:
+
+-Imprime "Hola mundo"
+-Muestra la fecha y hora acual
+-Permite crear usuarios introduciendo nombre, apellidos y emali
+-Guarda los usuarios en un array y los muestra por pantalla
+
+# Estructura del proyecto
+
+-Principal.java: contiene el programa principal y la logica
+-Usuario.java: define la clase Usuario
+
+#Ramas utilizadas 
+
+-main: version estable del proyecto
+-Practica4_1: añade la funcionalidad de fecha y hora
+-Practica4_1B: añade gestion de usuarios
